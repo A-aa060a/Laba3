c82e69a3a4b44e241dc04a2fe0ad19c267cb330a
A-aa060a <aaaaaa16193@gmail.com>
2023-12-18 14:37:52 +0300

    f

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/FragmentViewModel.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/FragmentViewModel.java
@@ -1,2 +1,25 @@
-package com.bignerdranch.android.beatbox;public class FragmentViewModel {
+package com.bignerdranch.android.beatbox;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.ObservableField;
+import android.widget.SeekBar;
+
+import java.text.MessageFormat;
+
+public class FragmentViewModel extends BaseObservable {
+
+    public ObservableField<String> mPlaybackSpeedRate = new ObservableField<>("Скорость воспроизведения: 100 %");
+    private BeatBox mBeatBox;
+
+    public FragmentViewModel(BeatBox beatBox) {
+        if (beatBox != null) {
+            mBeatBox = beatBox;
+        }
+    }
+
+    public void onPlaybackSpeedRateChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mPlaybackSpeedRate.set(MessageFormat.format("Скорость воспроизведения: {0} %", progress + 20));
+        mBeatBox.setPlaybackSpeedRate((progress + 20.0f) / 100);
+    }
+
 }
